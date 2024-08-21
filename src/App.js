acef832ff25017cc3c9bd0a96f3ecd318bb78145
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-21 13:24:52 +0300

    creating the routes for the all pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Import your pages
+import Home from "./Pages/Home";
+import Startups from "./Pages/Startups";
+import Investors from "./Pages/Investors";
+import Mentors from "./Pages/Mentors";
+import AboutUs from "./Pages/AboutUs";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Startups />} />
+        <Route path="/startups" element={<Home />} />
+        <Route path="/investors" element={<Investors />} />
+        <Route path="/mentors" element={<Mentors />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
