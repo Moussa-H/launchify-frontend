ef97924f8d7cbeec50bfbc6996d369602e27a42b
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-23 00:46:45 +0300

    adding a hero banner to the about us page

--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import React from "react";
+import Layout from "../../Common/Layout";
+import "./style.css";
+import hero from "../../assets/hero-about-us.png"
 
 export default function AboutUs() {
-  return <div></div>;
+  return (
+    <div>
+      <Layout>
+        <div className="banner-section">
+          <img
+            src={hero} 
+            alt="Banner"
+            className="banner-image"
+          />
+          <div className="banner-content text-center">
+            <h1>Connect with the best-in-class</h1>
+            <h1>Startup founders, in seconds</h1>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
 }
