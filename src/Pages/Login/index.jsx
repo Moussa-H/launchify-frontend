41542f5edb3cbc4b806359a95507e66e4033881a
Moussa_Haidar <moussahaidar999@gmail.com>
2024-08-27 10:42:13 +0300

    creating Login page and finishing the design

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
-export default function Login() {
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import "./style.css"; // Import the CSS file
+import logo from "../../assets/logo.svg";
+const Login = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container
+      fluid
+      className="d-flex justify-content-center align-items-center Login-container"
+    >
+      <div className="Login-box">
+        {/* Logo */}
+        <div className="text-center mb-4">
+          <img src={logo} alt="Logo" className="Login-logo" />
+        </div>
+
+        <h3 class="text-center mb-5 custom-font-size">Login to your account</h3>
+
+        <Form>
+          <Form.Group controlId="formUsername" className="mb-4">
+            <Form.Control type="text" placeholder="Username" />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword" className="mb-4">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            type="submit"
+            className="Login-button w-100"
+          >
+            Login
+          </Button>
+        </Form>
+
+        {/* Login Link */}
+        <div className="text-center mt-3">
+          <span>Don’t have an account ? </span>
+          <a href="/sign-up" className="login-link">
+            Sign up
+          </a>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
