dfc7426f0813c14ef2b6fbbfbf90d7fde056b310
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-08 11:49:50 +0300

    Creating a component for the imageUpload

--- src/Components/ImageUpload/index.jsx
+++ src/Components/ImageUpload/index.jsx
@@ -1,44 +1,69 @@
 // ImageUpload.js
-import React from "react";
+import React, { useState, useRef } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const ImageUpload = ({
-  image,
-  dragging,
-  onDragOver,
-  onDragLeave,
-  onDrop,
-  onClick,
-  onChange,
-}) => (
-  <div className="row mb-4">
-    <div className="col-12 col-md-3 d-flex flex-column align-items-center justify-content-center">
-      <div
-        className={`upload-box ${dragging ? "dragging" : ""}`}
-        onDragOver={onDragOver}
-        onDragLeave={onDragLeave}
-        onDrop={onDrop}
-        onClick={onClick}
-      >
-        {image ? (
-          <img src={image} alt="Preview" className="img-fluid preview-image" />
-        ) : (
-          <div className="text-center">
-            {dragging ? (
-              <p>Drop the image here...</p>
-            ) : (
-              <p>Drag & Drop or Click to Upload</p>
-            )}
-          </div>
-        )}
-      </div>
+const ImageUpload = ({ image, setImage, setFormData }) => {
+  const [dragging, setDragging] = useState(false);
+  const fileInputRef = useRef(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+      setFormData((prevData) => ({ ...prevData, image: file }));
+    }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setDragging(false);
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+      setFormData((prevData) => ({ ...prevData, image: file }));
+    }
+  };
+
+  return (
+    <div
+      className={`upload-box ${dragging ? "dragging" : ""}`}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      onClick={() => fileInputRef.current.click()}
+    >
+      {image ? (
+        <img src={image} alt="Preview" className="img-fluid preview-image" />
+      ) : (
+        <div className="text-center">
+          {dragging ? (
+            <p>Drop the image here...</p>
+          ) : (
+            <p>Drag & Drop or Click to Upload</p>
+          )}
+        </div>
+      )}
+
+      {/* Hidden file input for image upload */}
       <input
         type="file"
-        id="uploadImage"
+        ref={fileInputRef}
         style={{ display: "none" }}
-        onChange={onChange}
+        onChange={handleImageChange}
       />
     </div>
-  </div>
-);
+  );
+};
 
 export default ImageUpload;
