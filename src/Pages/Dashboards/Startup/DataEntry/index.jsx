2fe3121a04daaac923e7410aa380052fa2b58a65
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-09 03:34:11 +0300

    Implement a design for the DataEntry page

--- src/Pages/Dashboards/Startup/DataEntry/index.jsx
+++ src/Pages/Dashboards/Startup/DataEntry/index.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Expenses from  "../../../../Components/Expenses";
+import Incomes from "../../../../Components/Incomes";
+
 
 const DataEntry = () => {
+  const [activeSection, setActiveSection] = useState("expenses");
+
   return (
     <div>
-      <h2>Data Entry</h2>
-      {/* Add your Data Entry component code here */}
+      {/* Button Section */}
+      <div className="row">
+        <div className="col-12 mt-4 text-center">
+          <Button
+            className={`custom-btn ms-2 ${
+              activeSection === "expenses" ? "active" : ""
+            }`}
+            onClick={() => setActiveSection("expenses")}
+          >
+            Expenses
+          </Button>
+          <Button
+            className={`custom-btn ms-5 ${
+              activeSection === "incomes" ? "active" : ""
+            }`}
+            onClick={() => setActiveSection("incomes")}
+          >
+            Incomes
+          </Button>
+        </div>
+      </div>
+
+ 
+      {activeSection === "expenses" && <Expenses />}
+      {activeSection === "incomes" && <Incomes />}
     </div>
   );
 };
