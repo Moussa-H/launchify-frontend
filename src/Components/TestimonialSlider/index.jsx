d6207c9eacf01a1e6d9c73f0e6c0b8978fb62711
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-18 06:11:24 +0300

    Creating a TestimponialSlider Component

--- src/Components/TestimonialSlider/index.jsx
+++ src/Components/TestimonialSlider/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 import img1 from "../../assets/testimonial-1.png";
-import img2 from "../../assets/testimonial-1.png";
-import img3 from "../../assets/testimonial-1.png";
+import img2 from "../../assets/profile1.png";
+import img3 from "../../assets/profile2.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -25,17 +25,13 @@ function TestimonialSlider() {
               </div>
               <div className="col-md-10">
                 <p className="testimonial-text fst-italic">
-                  "Launchify is the best platform I have ever used. It provides
-                  a unique deal flow not just with raw project information but
-                  also as the ultimate destination for accessing pitch decks,
-                  FAQs, or video pitches. With its extensive database, Vestbee
-                  goes beyond platforms like Dealroom or PitchBook, offering
-                  unique insights and seamless connections with founders
-                  otherwise unavailable. Vestbee's value goes way beyond the
-                  software, great UX, and a strong pipeline. Their committed,
-                  engaged, and responsive team, strong community, amazing
-                  events, and invaluable updates and market insights make this
-                  platform a must-go!"
+                  "Launchify is a standout platform offering a unique deal flow
+                  with access to pitch decks, FAQs, and video pitches, setting
+                  it apart from competitors. Its extensive database provides
+                  insights and connections beyond what platforms like Dealroom
+                  or PitchBook offer. With great UX, a strong pipeline, and a
+                  highly engaged team, Launchify excels in supporting founders
+                  through valuable updates, events, and market insights."
                 </p>
               </div>
             </div>
@@ -47,21 +43,16 @@ function TestimonialSlider() {
               <div className="col-md-2 text-center text-md-start">
                 <img src={img2} alt="Person" className="rounded-circle mb-3" />
                 <h5>Jane Smith</h5>
-                <p>Investor</p>
+                <p>Founder & CEO</p>
               </div>
               <div className="col-md-10">
                 <p className="testimonial-text fst-italic">
-                  "Launchify is the best platform I have ever used. It provides
-                  a unique deal flow not just with raw project information but
-                  also as the ultimate destination for accessing pitch decks,
-                  FAQs, or video pitches. With its extensive database, Vestbee
-                  goes beyond platforms like Dealroom or PitchBook, offering
-                  unique insights and seamless connections with founders
-                  otherwise unavailable. Vestbee's value goes way beyond the
-                  software, great UX, and a strong pipeline. Their committed,
-                  engaged, and responsive team, strong community, amazing
-                  events, and invaluable updates and market insights make this
-                  platform a must-go!"
+                  "Launchify has revolutionized how we connect with startups.
+                  The platform’s in-depth data, pitch deck access, and seamless
+                  communication tools offer more than just project information.
+                  Their proactive team, insightful updates, and engaging events
+                  create a strong support network that goes beyond other
+                  platforms."
                 </p>
               </div>
             </div>
@@ -73,21 +64,15 @@ function TestimonialSlider() {
               <div className="col-md-2 text-center text-md-start">
                 <img src={img3} alt="Person" className="rounded-circle mb-3" />
                 <h5>Michael Lee</h5>
-                <p>Mentor & Advisor</p>
+                <p>Founder & CEO</p>
               </div>
               <div className="col-md-10">
                 <p className="testimonial-text fst-italic">
-                  "Launchify is the best platform I have ever used. It provides
-                  a unique deal flow not just with raw project information but
-                  also as the ultimate destination for accessing pitch decks,
-                  FAQs, or video pitches. With its extensive database, Vestbee
-                  goes beyond platforms like Dealroom or PitchBook, offering
-                  unique insights and seamless connections with founders
-                  otherwise unavailable. Vestbee's value goes way beyond the
-                  software, great UX, and a strong pipeline. Their committed,
-                  engaged, and responsive team, strong community, amazing
-                  events, and invaluable updates and market insights make this
-                  platform a must-go!"
+                  "StartupLink delivers unmatched value by providing deep
+                  insights, access to key startup materials, and smooth
+                  connections. The user-friendly interface, coupled with expert
+                  market insights and a dedicated community, makes it the
+                  perfect platform for founders and investors alike."
                 </p>
               </div>
             </div>
