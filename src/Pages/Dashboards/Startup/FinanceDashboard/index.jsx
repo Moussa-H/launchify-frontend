7b5584be8b819f02111c368d2e283091ba8366c7
Moussa_Haidar <moussahaidar999@gmail.com>
2024-09-18 05:54:21 +0300

    Creating a Finance Dashboard that have 2 sub menu inside the startupDashboard

--- src/Pages/Dashboards/Startup/FinanceDashboard/index.jsx
+++ src/Pages/Dashboards/Startup/FinanceDashboard/index.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import FinancialCards from "../../../../Components/FinancialCards"; // Adjust the path if necessary
+import MonthlyBreakdownChart from "../../../../Components/MonthlyBreakdownChart";
+import "./style.css";
+import { Container, Row, Col } from "react-bootstrap";
+import ExpensesTable from "../../../../Components/ExpensesTable";
+import GraphIncomes from "../../../../Components/GraphIncomes";
+const token = localStorage.getItem("token");
 
 const FinanceDashboard = () => {
   return (
     <div>
-      <h2>Finance Dashboard</h2>
-      {/* Add your Finance Dashboard component code here */}
+      <Container className="dashboard-container">
+        <FinancialCards token={token} />
+        <div className="chart-container">
+          <MonthlyBreakdownChart token={token} />
+        </div>
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <div className="chart-container">
+              <ExpensesTable token={token} />
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className="pie-container">
+              <GraphIncomes token={token} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
